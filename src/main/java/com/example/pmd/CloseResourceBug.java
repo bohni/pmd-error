06d61d93241d501d919586b6592bb850ecf82493
Stefan Bohn <stefan.bohn@shks.de>
2020-03-06 12:16:24 +0100

    Solution suposed by adangel

--- src/main/java/com/example/pmd/CloseResourceBug.java
+++ src/main/java/com/example/pmd/CloseResourceBug.java
@@ -4,13 +4,31 @@ import java.io.IOException;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+
 public class CloseResourceBug
 {
 
 	public void falsePositive() throws IOException
 	{
 		XSSFWorkbook workbook = new XSSFWorkbook();
+		try
+		{
+			// do stuff, which could raise a runtime exception
+		}
+		finally
+		{
+			workbook.close();
+		}
 
-		workbook.close();
+		// alternative with try-with-resources
+		try (XSSFWorkbook w2 = new XSSFWorkbook())
+		{
+			// do stuff, which could raise a runtime exception
+		}
+
+		// wrong close without exception handling
+//		XSSFWorkbook workbook = new XSSFWorkbook();
+		// do stuff, which could raise a runtime exception
+//		workbook.close();
 	}
 }
